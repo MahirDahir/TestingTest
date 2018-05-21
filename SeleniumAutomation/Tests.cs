4d318758b8a42bedfc23bc7e4b9df5770e6024d5
Muhannad Orabi <muhannado@codevalue.net>
2018-05-21 14:48:58 +0300

    initial draft

---
+++ SeleniumAutomation/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SeleniumAutomation
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void Bla()
+        {
+
+        }
+    }
+}
